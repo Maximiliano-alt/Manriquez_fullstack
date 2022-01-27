e80355857900f2e9d894bb18484b006e5ed35fdc
CheloNeo <maemguitarra@gmail.com>
2022-01-26 21:46:39 -0300

    docs(auth):modify-structure-auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './login/components/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
 
   ],
   imports: [
