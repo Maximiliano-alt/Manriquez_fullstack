75f28cf37e3538081f3a9a6a9050c4f7d82f5121
CheloNeo <maemguitarra@gmail.com>
2022-01-28 17:18:24 -0300

    style(logincss):modify-css-login

--- src/app/auth/pages/login/components/login.component.ts
+++ src/app/auth/pages/login/components/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormControl } from '@angular/forms';
+import { User } from 'src/app/services/create-user.service';
 
 
 @Component({
@@ -9,6 +10,11 @@ import { Validators, FormControl } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
+  user:User = {
+    rut:'',
+    password:'',
+  }
+
   rut: FormControl;
   pass: FormControl;
   checkbox:FormControl;
@@ -17,21 +23,22 @@ export class LoginComponent implements OnInit {
     this.rut = new FormControl('',[
       Validators.required,
       Validators.minLength(9),
-      Validators.maxLength(10)
+      Validators.maxLength(10),
+      Validators.pattern(/^[0-9].-*$/)
     ]);
     this.rut.valueChanges.subscribe(
       value =>{
-        console.log(value);
+        this.user.rut = value
       }
     );
     this.pass = new FormControl('',[//20636614-1
       Validators.required,
       Validators.minLength(8),
-      Validators.pattern(/^[A-Z].[a-z].[0-9].*$/)
+      Validators.pattern(/^[A-Z].*[a-z].[0-9].*$/)
     ]);
     this.pass.valueChanges.subscribe(
       value =>{
-        console.log(value);
+        this.user.password = value
       }
     );
     this.checkbox = new FormControl();
@@ -45,11 +52,21 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
   saveInfo(event:Event){
+   
     event.preventDefault();
     if(this.rut.valid && this.pass.valid){
       console.log(this.rut.value);
       console.log(this.pass.value);
     }
     console.log('envio no valido')
+  }
+
+  logIn(data:User){
+    if(data.password=='' || data.rut == ''){
+      console.log("Ingreso no valido!")
+    }
+    else{
+      console.log(data)
+    }
   }
 }
