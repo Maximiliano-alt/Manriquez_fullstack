e8271baf7d0759a964f1612398ee5aef72190586
CheloNeo <maemguitarra@gmail.com>
2022-01-29 17:59:49 -0300

    feat(addVentas):add-component-ventas

--- src/app/ventas/pages/list-venta/list-venta.component.ts
+++ src/app/ventas/pages/list-venta/list-venta.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,29 +12,83 @@ export class ListVentaComponent implements OnInit {
   inicio_slice = 0;
   index_aux = 0;
   resto=0;
+  final = 0;
   array=[
-    1,2,3,4,5,6,7,8,9,10
+    1,2,3,4,5,6,7,8,9,10,11,12,13,
   ]
+
+  inicio:any=0;
+
+
   segmento:any=[]
   constructor() { }
 
   ngOnInit(): void {
-    this.resto = this.array.length%4;
-    console.log(this.resto)
+   this.resto = this.array.length%4
+   this.nf_for_next();
     
   }
 
-  nf_for_next(){
-    // this.segmento=[]
-    this.segmento=[]  
-    this.array.splice(this.inicio_slice,this.indice_espera).forEach(element=>{
-      this.segmento.push(element);
-      this.array.push(element)
-    })
-    console.log(this.segmento)
-      
+  nf_for_next():any{
+    var aux = this.segmento;
+    this.segmento = [];
+
+
+    if(this.inicio_slice == (this.array.length - this.resto) && this.resto != 0){
+
+      for (let index = (this.array.length-this.resto); index < this.array.length; index++) {
+        this.segmento.push(this.array.find(element=>element==this.array[index])) 
+      }
+   
+      return 0;
+    }
+    else{
+
+      // recorremos del principio hasta llegar al resto
+
+      if(this.resto == 0 && this.indice_espera == this.array.length+4){
+        
+        this.segmento = aux;
+        return 0;
+      }
+      else{
+        for (let index = this.inicio_slice; index < this.indice_espera; index++) {
+          this.segmento.push(this.array.find(element=>element==this.array[index])) 
+          
+        }
+        //aniadimos 4 unidades par poder recorrer el ng-for con 4 ventas
+        this.indice_espera = this.indice_espera + 4;
+        this.inicio_slice = this.inicio_slice + 4;  
+        return 1;
+      }
     }
     
+    
+  
+      
+  }
+  
+  nf_for_preview():any{
+    var aux = this.segmento;
+    this.segmento = [];
+    
+    
+    if(this.indice_espera -4 <=0){
+      this.segmento = aux;
+      return 0;
+    }
+    else{
+      this.inicio_slice = this.inicio_slice - 4;
+      this.indice_espera = this.indice_espera -4;
+      for (let index = this.inicio_slice; index < this.indice_espera; index++) {
+        this.segmento.push(this.array.find(element=>element==this.array[index])) 
+        
+      }
+      return 1;
+    }
+    
+   
+  }
 
 
 }
