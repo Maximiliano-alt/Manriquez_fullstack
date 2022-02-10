de6beffec608d0dbad44b168ff70c6b77aed36dc
CheloNeo <maemguitarra@gmail.com>
2022-02-10 17:57:36 -0300

    style(css-client-graphics):modify-css-client-and-ts

---
+++ src/app/clientes/service/cliente.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor(private http: HttpClient ) { }
+
+
+  add_cliente(data:cliente){
+    return this.http.post(environment.baseUrl+'/createCliente',data);
+  }
+
+}
+
+export interface cliente{
+  nombre: string,
+  direccion: String,
+  telefono: String,
+  correo: String,
+  rut: String,
+  totalDeCompra:Number,
+  historial:
+  //historial de compras
+  []
+}
