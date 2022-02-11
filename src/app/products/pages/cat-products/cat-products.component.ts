4c50b2ebc877aa4974a6feb35909e09d831c941d
eliezer-zuniga <eliezer.zuniga@alumnos.uv.cl>
2022-02-11 18:26:03 -0300

    feature(cat-products/css): feature category-products design with responsive and and routes was modified in products module.

---
+++ src/app/products/pages/cat-products/cat-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-products',
+  templateUrl: './cat-products.component.html',
+  styleUrls: ['./cat-products.component.css']
+})
+export class CatProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
