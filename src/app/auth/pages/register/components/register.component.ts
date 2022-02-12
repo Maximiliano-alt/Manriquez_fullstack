73b4492820f816d04a7359441cc4514bfbeacdc0
Maximiliano-alt <74783377+Maximiliano-alt@users.noreply.github.com>
2022-02-12 00:16:10 -0300

    feat(register/component): form control added

--- src/app/auth/pages/register/components/register.component.ts
+++ src/app/auth/pages/register/components/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { User } from 'src/app/services/create-user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  name: FormControl;
+  rut: FormControl;
+  email:FormControl;
+  pass:FormControl;
+  rePass:FormControl;
+
+  user:User = {
+    rut:'',
+    password:'',
+  }
+
+
+  constructor() {
+    this.name = new FormControl('',[
+      Validators.required,
+    ]);
+    this.name.valueChanges.subscribe(
+      value =>{
+        this.user.rut = value
+      }
+    );
+
+    this.rut = new FormControl('',[
+      Validators.required,
+      Validators.minLength(9),
+      Validators.maxLength(10),
+      Validators.pattern(/^[0-9].-*$/)
+    ]);
+    this.rut.valueChanges.subscribe(
+      value =>{
+        this.user.rut = value
+      }
+    );
+
+    this.email = new FormControl('',[
+      Validators.required,
+      Validators.email
+    ]);
+    this.email.valueChanges.subscribe(
+      value =>{
+        this.user.rut = value
+      }
+    );
+
+
+    this.pass = new FormControl('',[//20636614-1
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/^[A-Z].*[a-z].[0-9].*$/)
+    ]);
+    this.pass.valueChanges.subscribe(
+      value =>{
+        this.user.password = value
+      }
+    );
+
+    this.rePass = new FormControl('',[//20636614-1
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/^[A-Z].*[a-z].[0-9].*$/)
+    ]);
+    this.rePass.valueChanges.subscribe(
+      value =>{
+        this.user.password = value
+      }
+    );
+  }
 
   ngOnInit(): void {
   }
