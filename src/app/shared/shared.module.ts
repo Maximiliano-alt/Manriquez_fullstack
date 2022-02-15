79c1b1123725432d48ab41b3c3a58fd9e3b98ab4
CheloNeo <maemguitarra@gmail.com>
2022-02-15 11:19:45 -0300

    test(list-componet):modify-shared-module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,34 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
-<<<<<<< HEAD
 import { ListaComponent } from './lista/lista.component';
 import { MaterialModule } from '../material/material.module';
-@NgModule({
-  declarations: [
-    NavbarComponent,
-    ListaComponent
-    
-=======
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    NavbarComponent
-
->>>>>>> 5f8fa155c8be2eab6474acb5fe394a479996a4d0
+    NavbarComponent,
+    ListaComponent,
   ],
 
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
-<<<<<<< HEAD
-    MaterialModule
-    
-=======
+    MaterialModule,    
     RouterModule
->>>>>>> 5f8fa155c8be2eab6474acb5fe394a479996a4d0
   ],
   exports:[
     NavbarComponent,
