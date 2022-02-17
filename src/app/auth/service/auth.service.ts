e0f8aff1fd221e075ac6d30f7996cb221e1876a8
CheloNeo <maemguitarra@gmail.com>
2022-02-17 01:42:56 -0300

    test(loginService):modify-register

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -10,18 +10,36 @@ export class AuthService {
 
   constructor(private http: HttpClient,private cookie: CookieService) { }
 
-  login(data:user){
+  login(data:userLogin){
     var aux = this.http.post<res>(environment.baseUrl+'/login',data)
-    
+    console.log(this.cookie.get('_pipo'))
     aux.subscribe(
       res=>{
-        this.cookie.set('token',res.mensaje)
+        var date = Date.now();
+        var expired = new Date(date).toString().split(" ")
+        var dateTime= expired[4].split(":")
+        const newMinuto =parseInt( dateTime[1])+1
+        dateTime[1] = newMinuto.toString()
+        console.log(dateTime)
+        this.cookie.set('_pipo',res.mensaje)
       }
     )
     return {status:200,mensaje:"login exitoso"}
   }
+
+  register(data:user){
+    return this.http.post<res>(environment.baseUrl+'/new',data)
+  }
+
+
 }
 export interface user{
+  nombre:string,
+  rut:string,
+  email:string,
+  pass:string,
+}
+export interface userLogin{
   rut:string,
   pass:string,
 }
