d3da8ce679e610548d8da6175e5e36f9b730874b
Juan Larenas <jalarenasv@gamil.com>
2022-02-21 23:18:56 -0300

    feat(proveedores): arrow function

--- src/app/proveedores/pages/new-proveedor/new-proveedor.component.ts
+++ src/app/proveedores/pages/new-proveedor/new-proveedor.component.ts
@@ -4,15 +4,27 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-new-proveedor',
   templateUrl: './new-proveedor.component.html',
   styleUrls: ['./new-proveedor.component.css'],
+  host: {
+    '(window:resize)': 'onResize($event)',
+  },
 })
 export class NewProveedorComponent implements OnInit {
-  view: boolean = true;
-  constructor() {}
+  view: boolean = false;
+  width:number= 0;
+  widthBoolean : boolean = false;
+  constructor() {
+    this.width = screen.width;
+  }
 
   ngOnInit(): void {}
 
   cambiarView() {
     this.view = !this.view;
-  
+    if(this.width >=1024) {this.widthBoolean = false;}
+    else this.widthBoolean = true;  
+  }
+
+  onResize(event: any) {
+    this.width = event.target.innerWidth;
   }
 }
