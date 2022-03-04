401cce8dcd7c842a90728be210e75cb6c749d01e
Maximiliano-alt <74783377+Maximiliano-alt@users.noreply.github.com>
2022-03-04 00:23:44 -0300

    feat(pdf-service): date time added

--- src/app/ventas/services/pdf.service.ts
+++ src/app/ventas/services/pdf.service.ts
@@ -6,11 +6,17 @@ import * as pdfFonts from 'pdfmake/build/vfs_fonts';
   providedIn: 'root'
 })
 export class PdfService {
+  dateTime: Date;
 
   constructor() {
+    this.dateTime = new Date();
     (window as any).pdfMake.vfs = pdfFonts.pdfMake.vfs;
    }
 
+  transformMonth(date: Date):string {
+    const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    return monthNames[date.getMonth()];
+  }
   async downloadPdf(type:string){
     const pdf = new PdfMakeWrapper();
     pdf.pageSize('A4');
@@ -21,7 +27,11 @@ export class PdfService {
   });
 
     pdf.add( await new Img('../assets/page/Pisosmanriquez.logo-02.png').width(200).build());
-    pdf.add(new Txt('\n\nME Construcción y Diseño de interiores Spa').color('orange').relativePosition(40,-17).end);
+    pdf.add(new Txt('\n\nME Construcción y Diseño de interiores Spa').color('#F25C05').relativePosition(0,-20).bold().end);
+    pdf.add(new Txt('\n\n76.861.179-3').color('#F25C05').relativePosition(0,-5).bold().end);
+    pdf.add(new Txt('\n\nSantiago. '+this.dateTime.getUTCDate()+ ' de '+this.transformMonth(this.dateTime)+' del '+this.dateTime.getFullYear()).decoration('underline').relativePosition(300,45).bold().end);
+
+
     pdf.create().open();
   }
 }
