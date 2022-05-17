2266c8d1dc10310d01221f66bbf237b281024058
CheloNeo <maemguitarra@gmail.com>
2022-05-17 00:29:14 -0400

    new commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://pisos-manriquez.herokuapp.com/api',
+  baseUrl: 'https://appmanriquez.onrender.com/api',
   // baseUrl:'http://localhost:4000/api', 
 };
